5a7af027dd7180a9e910ae07e8f61ceb4978ad44
Daniel Covelli <covellidanny@gmail.com>
2020-06-11 12:34:16 -0700

    initial commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Bib Williams',
+    email: 'bob@gmail',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Shannon Jackson',
+    email: 'shannon@gmail.com',
+    status: 'active'
+  }
+];
+
+module.exports = members;
